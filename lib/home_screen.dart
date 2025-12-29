60eef27eda1da34c2279572dfc770403e23bcf8f
rahul <meetmeerahul@gmail.com>
2025-12-29 11:02:38 +0530

    Dialoge box,bottom sheet dark and light theme

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -14,10 +14,77 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       appBar: AppBar(title: Text("Getx Mastering")),
 
-      body: Column(children: [
+      body: Column(
+        children: [
+          Card(
+            child: ListTile(
+              title: Text("GetX"),
+              subtitle: Text("Getx Dialog Alert"),
+              onTap: () {
+                Get.defaultDialog(
+                  title: "Delete Chat",
+                  titlePadding: .all(20),
+                  middleText: "Are you sure want to delete this chat??",
+                  contentPadding: .all(20),
+                  // content:  here we can design the dialoge,
+                  confirm: TextButton(
+                    onPressed: () {
+                      // Navigator.pop(context);
+                      Get.back();
+                      Get.snackbar("Success", "Chat Deleted");
+                    },
+                    child: Text("Ok"),
+                  ),
+                  cancel: TextButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: Text("Cancel"),
+                  ),
+                );
+              },
+            ),
+          ),
 
-      ],
-    ),
+          Card(
+            child: ListTile(
+              title: Text("GetX"),
+              subtitle: Text("Getx Bottomsheet"),
+              onTap: () {
+                Get.bottomSheet(
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.teal,
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+
+                    child: Column(
+                      children: [
+                        ListTile(
+                          leading: Icon(Icons.light),
+                          title: Text("Light Theme"),
+                          onTap: () {
+                            Get.changeTheme(ThemeData.light());
+                            Get.back();
+                          },
+                        ),
+                        ListTile(
+                          leading: Icon(Icons.dark_mode),
+                          title: Text("Dark Theme"),
+                          onTap: () {
+                            Get.changeTheme(ThemeData.dark());
+                            Get.back();
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Get.snackbar(
