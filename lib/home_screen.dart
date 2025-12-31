94a818439bb91d0f1780b0d5f1203c7dd87f5c1c
rahul <meetmeerahul@gmail.com>
2025-12-31 09:52:21 +0530

    State management with getx - counter

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:learning_getx/screen_one.dart';
+import 'package:learning_getx/counter_controller.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -10,48 +10,20 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final CounterController controller = Get.put(CounterController());
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Getx Mastering")),
-
-      body: Column(
-        mainAxisAlignment: .center,
-        crossAxisAlignment: .center,
-        children: [
-          ListTile(title: Text('message'.tr), subtitle: Text("name".tr)),
-
-          Row(
-            children: [
-              TextButton(
-                onPressed: () {
-                  Get.updateLocale(Locale('en', 'US'));
-                },
-                child: Text("English"),
-              ),
-              TextButton(
-                onPressed: () {
-                  Get.updateLocale(Locale('hi', 'IN'));
-                },
-                child: Text("Hindi"),
-              ),
-              TextButton(
-                onPressed: () {
-                  Get.updateLocale(Locale('ml', 'IN'));
-                },
-                child: Text("Malayalam"),
-              ),
-            ],
-          ),
-        ],
+      body: Center(
+        child: Obx(() {
+          return Text(controller.counter.toString());
+        }),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          Get.snackbar(
-            "GetX",
-            "Your have taken the first step",
-            isDismissible: true,
-          );
+          controller.incrementCounter();
         },
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import 'package:learning_getx/home_screen.dart';
-import 'package:learning_getx/languages.dart';
-import 'package:learning_getx/screen_one.dart';
-import 'package:learning_getx/screen_two.dart';
 
 void main() {
   runApp(const MyApp());
@@ -19,18 +16,9 @@ class MyApp extends StatelessWidget {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      locale: Locale('en', 'US'),
-      fallbackLocale: Locale('en', 'US'),
-      translations: Languages(),
+
       theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
       home: const HomeScreen(),
-      // getPages: [
-      //   GetPage(name: '/', page: () => HomeScreen()),
-
-      //   GetPage(name: '/ScreenOne', page: () => ScreenOne()),
-
-      //   GetPage(name: '/ScreenTwo', page: () => ScreenTwo()),
-      // ],
     );
   }
 }
